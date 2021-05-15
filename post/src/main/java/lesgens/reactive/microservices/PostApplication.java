24a0e517ba052dd8f1ac847f974cc75caaffed53
verzelea <arthurverzele@hotmail.com>
2021-05-15 22:34:03 +0200

    Error: Failed to configure a DataSource

--- post/src/main/java/lesgens/reactive/microservices/PostApplication.java
+++ post/src/main/java/lesgens/reactive/microservices/PostApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class SocialmediaApplication {
+public class PostApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SocialmediaApplication.class, args);
+		SpringApplication.run(PostApplication.class, args);
 	}
 
 }
