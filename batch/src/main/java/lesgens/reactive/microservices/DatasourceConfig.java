9d7953116843525ea79cffb4b7700b91e2cff472
Skiadram <nlcoffin@free.fr>
2021-05-16 18:44:34 +0200

    :card_file_box: add database struct into batch

---
+++ batch/src/main/java/lesgens/reactive/microservices/DatasourceConfig.java
@@ -0,0 +1,64 @@
+package lesgens.reactive.microservices;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceConfig {
+
+    @Primary
+    @Bean
+    @ConfigurationProperties(prefix = "datasource.post")
+    public DataSource lessonsDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "datasource.post.liquibase")
+    public LiquibaseProperties lessonsLiquibaseProperties() {
+        return new LiquibaseProperties();
+    }
+
+    @Bean
+    public SpringLiquibase lessonsLiquibase() {
+        return springLiquibase(lessonsDataSource(), lessonsLiquibaseProperties());
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "datasource.user")
+    public DataSource notationsDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "datasource.user.liquibase")
+    public LiquibaseProperties notationsLiquibaseProperties() {
+        return new LiquibaseProperties();
+    }
+
+    @Bean
+    public SpringLiquibase notationsLiquibase() {
+        return springLiquibase(notationsDataSource(), notationsLiquibaseProperties());
+    }
+
+    private static SpringLiquibase springLiquibase(DataSource dataSource, LiquibaseProperties properties) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog(properties.getChangeLog());
+        liquibase.setContexts(properties.getContexts());
+        liquibase.setDefaultSchema(properties.getDefaultSchema());
+        liquibase.setDropFirst(properties.isDropFirst());
+        liquibase.setShouldRun(properties.isEnabled());
+        liquibase.setLabels(properties.getLabels());
+        liquibase.setChangeLogParameters(properties.getParameters());
+        liquibase.setRollbackFile(properties.getRollbackFile());
+        return liquibase;
+    }
+}
